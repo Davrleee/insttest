dfdaf2b0817302f2d6f679d40b2e937f09905a0f
dominator00700 <dominatordlk@gmail.com>
2024-11-27 14:37:32 +0100

    prisma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import NavBar from "../components/NavBar";
+
 
 export const metadata: Metadata = {
-  title: "SnapZoška",
-  description: "Made by Dominik",
+  title: "InstaMain",
+  description: "Vytvorené Dominikom",
 };
 
 export default function RootLayout({
@@ -14,10 +16,13 @@ export default function RootLayout({
   return (
     <html lang="sk">
       <body>
-        {children}
+        <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+          <main style={{ flexGrow: 1 }}>
+            {children}
+          </main>
+          <NavBar /> 
+        </div>
       </body>
     </html>
   );
 }
-
-
